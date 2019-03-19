9233d9711073740805612f2a08f7621ec82742f6
PinkFallenUnicorn <ObeyTheCaretaker@yandex.by>
2019-03-20 02:04:05 +0300

    Some modern opengl experience

--- dev.cpp
+++ dev.cpp
@@ -1,6 +1,10 @@
+#include <GL/glew.h>
+#include <GL/glut.h>
 #include <iostream>
 #include <vector>
 #include <GLFW/glfw3.h>
+
+
 
 int main(void)
 {
@@ -9,6 +13,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+    
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1000, 1000, "Hello World", NULL, NULL);
@@ -20,7 +25,20 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+    
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.3f,   0.5f,
+         0.5f,  -0.5f,
 
+    };
+
+    uint32_t buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float));
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
