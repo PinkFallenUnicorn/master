34d0f8d1f8689a618debd6950dedd0a84aa6e3f6
PinkFallenUnicorn <ObeyTheCaretaker@yandex.by>
2019-03-31 01:32:44 +0000

    Added vertex/fragment shaders as shader.vs/frag, changed main.cpp: removed legacy shaders stuff, included new shaders. Program works good, so - checkpoint, soon I'll merge it to master

--- main.cpp
+++ main.cpp
@@ -3,24 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
+#include "shader.h"
 
-const GLchar* vertexShaderSource = "#version 420 core\n"
-        "layout (location = 0) in vec3 position;\n"
-        "layout (location = 1) in vec3 color;\n"
-        "out vec3 ourColor;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-        "    ourColor = color;\n"
-        "}\n\0";
-const GLchar* fragmentShaderSource = "#version 420 core\n"
-        "in vec3 ourColor;\n"
-        "out vec4 color;\n"
-        //"uniform vec4 ourFirstColor;\n"
-        "void main()\n"
-        "{\n"
-        "   color = vec4(ourColor, 1.0f);\n"
-        "}\n\0";
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
@@ -66,54 +50,7 @@ int main()
     
 
 
-    GLuint vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    
-    GLuint fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if(!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-
-    
-
-    GLuint shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
-    
-
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader myShader ("shader.vs", "shader.frag");
 
     GLfloat vertices1[] = {
         -0.5f, -0.5f, 0.0f,  0.0f,  0.0f, 1.0f,
@@ -166,9 +103,7 @@ int main()
         glfwPollEvents();
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        glUseProgram(shaderProgram);
-
+        myShader.Use();
 
         glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
